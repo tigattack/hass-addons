aca105860ac21bac0e22a8c47d62d3f83f88d55f
tigattack <10629864+tigattack@users.noreply.github.com>
2025-11-14 00:07:56 +0000

    refactor(smartctl_exporter): update launch scripts

---
+++ smartctl_exporter/rootfs/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bashio
+# ==============================================================================
+# Home Assistant Community Add-on: smartctl exporter
+# Runs smartctl_exporter
+# ==============================================================================
+
+# Start smartctl_exporter
+exec /etc/services.d/smartctl_exporter/run
